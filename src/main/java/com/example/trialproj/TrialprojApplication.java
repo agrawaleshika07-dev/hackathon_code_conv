242f28328413ef2e8facaff2c3d79989f831bb8e
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-02 23:22:49 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/trialproj/TrialprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.trialproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrialprojApplication.class, args);
+    }
+
+}
