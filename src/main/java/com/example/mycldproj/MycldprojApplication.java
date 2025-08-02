06aaa055ca40eeee1b259ffb0a356ee65833f2aa
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-02 23:07:03 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/mycldproj/MycldprojApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycldproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycldprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycldprojApplication.class, args);
+    }
+
+}
+
