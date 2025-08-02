bdfac7d25c0d5b9ca46d17344673019bee4e4833
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-02 23:07:04 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/mycldproj/service/HelloWorldService.java
@@ -0,0 +1,12 @@
+package com.example.mycldproj.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
+
